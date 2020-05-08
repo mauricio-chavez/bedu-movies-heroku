2637b47fcacbb1eb16ff4dc09fe0ef143f18fb9d
Mauricio Chávez Olea <mauriciochavez@ciencias.unam.mx>
2020-05-07 20:18:05 -0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,51 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const { body } = require('express-validator');
+const MovieModel = require('./models/movie')
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+const app = express()
+
+// Middleware
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.get('/', async (req, res) => {
+  const movies = await MovieModel.find()
+  res.json(movies)
+})
+
+app.get('/:id', async (req, res) => {
+  const { id } = req.params
+  const movie = await MovieModel.findById(id)
+  res.json(movie)
+})
+
+app.post('/', [
+  body('title').isString().not().isEmpty(),
+  body('director').isString(),
+  body('productionBudget').isNumeric(),
+  body('releaseDate').isString(),
+  body('rottenTomatoesRating').isFloat(),
+  body('imdbRating').isFloat(),
+], async (req, res) => {
+  const movie = await MovieModel.create(req.body)
+  res.json(movie)
+})
+
+const server = app.listen(process.env.PORT || 3000, async () => {
+  try {
+    await mongoose.connect(process.env.DATABASE_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    })
+    console.log(`Listening on ${server.address().port}`)
+  } catch(e) {
+    console.log(e)
+    console.log('Error while connecting to database')
+    process.exit(1)
+  }
+})
